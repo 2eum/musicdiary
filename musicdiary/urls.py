984a474d14622cee43143a7eec811e5209842a39
koowoy <uk960214@gmail.com>
2021-08-05 22:48:54 +0900

    listview page complete

--- musicdiary/urls.py
+++ musicdiary/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import user_listview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', user_listview)
 ]

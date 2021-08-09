9bb0c0a0d8dc81276a4b0ba09b831f97168f31fb
kjy1101 <amelia1023@aver.com>
2021-08-09 16:10:12 +0900

    edit에서도 음악 바꿀 수 있음

--- posts/views.py
+++ posts/views.py
@@ -70,17 +70,27 @@ def detail(request, index):
 
 def edit(request, index):
     post = get_object_or_404(Content, pk=index)
+    
+    track_title = request.POST.get('track_title')
+    track_artist = request.POST.get('track_artist')
+    track_album_cover = request.POST.get('track_album_cover')
+    track_audio = request.POST.get('track_audio')
+
     if request.method == "POST":
         form = ContentForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
+            post.track_title = track_title
+            post.track_artist = track_artist
+            post.track_album_cover = track_album_cover
+            post.track_audio = track_audio
             post.author = request.user
             post.published_date = timezone.now
             post.save()
             return redirect('detail', index=post.pk)
     else:
         form = ContentForm(instance=post)
-    return render(request, 'edit.html', {'form':form})
+    return render(request, 'edit.html', {'form':form, 'post':post})
 
 def delete(request, pk):
     post = get_object_or_404(Content, pk=pk)

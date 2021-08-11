e528fea434148982d84475feeae459d942aa65fd
koowoy <uk960214@gmail.com>
2021-08-11 15:34:56 +0900

    set debug false

--- musicdiary/settings.py
+++ musicdiary/settings.py
@@ -28,7 +28,7 @@ CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ileum.herokuapp.com', '127.0.0.1']
 

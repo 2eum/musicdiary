e0d933e6687d7453e65160cd7f89d1a1d91a11c2
koowoy <uk960214@gmail.com>
2021-08-11 21:18:53 +0900

    change static settings

--- musicdiary/settings.py
+++ musicdiary/settings.py
@@ -125,11 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'posts/static'),
 )
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
